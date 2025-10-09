4794d975f207a12046884036b2f05dd1817940f0
NasirHussainSajidi <nasirsajidi313@gmail.com>
2025-10-09 23:42:05 +0500

    class-34

---
+++ class-34/app.js
@@ -0,0 +1,29 @@
+// Closures 
+
+
+// A function along with it's lexical environment its called a closure.
+
+function z() {
+    var a = 10;
+   return function x() {
+        var b = 20;
+
+        return function y() {
+            console.log(a, b);
+        }
+        // y()
+    }
+}
+
+var abc = z()
+
+var def = abc();
+
+def()
+
+
+
+
+
+
+// z()
